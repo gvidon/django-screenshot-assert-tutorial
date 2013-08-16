faf88aa45907ae3038d28ee56aa10025dedf0bae
nide <nenegoro@gmail.com>
2013-08-17 02:15:26 +0400

    Stepan is now Ghost

--- webdriver.py
+++ webdriver.py
@@ -1,35 +1,23 @@
 import sys
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.webdriver import WebDriver
-from needle.driver import NeedleWebDriver
+from django.conf import settings
+from ghost import Ghost
 
 
-# NeedleWebDriver is required as it provide WebElement with .get_screenshot() method
-# which is used for page ELEMENTS (not the whole page) capturing
-class Stepan(NeedleWebDriver, WebDriver):
-	def __init__(self, *args, **kwargs):
-		super(Stepan, self).__init__(*args, **kwargs)
-		
-		# This tool is used for waiting for WebDriver certain events described in lambda
-		# passed to it's methods
-		self.wait = WebDriverWait(self, 30)
-
+class Stepan(Ghost):
 	# Login and wait for response
 	def login(self, url, username, password):
-		super(WebDriver, self).get(url)
+		super(Stepan, self).open(url)
 
 		# Fill up and submit login form
-		userinput = self.find_element_by_css_selector('input[name="username"]')
+		self.fill('form', {
+			'username': settings.TEST_USER,
+			'password': settings.TEST_PASSWORD
+		})
 
-		userinput.send_keys(username)
-		self.find_element_by_css_selector('input[name="password"]').send_keys(password)
-		userinput.submit()
-
-		# .container is main content part. As it is visible page is considered to be loaded
-		self.wait.until(lambda D: D.find_element_by_css_selector('.container').is_displayed())
+		self.fire_on('form', 'submit', expect_loading=True)
 
 	# Go to URL and wait before page loaded including js-driven loading
-	def get(self, url):
-		super(WebDriver, self).get(url)
-		self.wait.until(lambda D: D.find_element_by_css_selector('#module-content').is_displayed())
+	def open(self, url):
+		super(Stepan, self).open(url)
+		self.wait_for_selector("#module-content:not(.hidden)")
