a1385ff47c8e9e8c8b555d716d4df73e787eb20e
nide <nenegoro@gmail.com>
2013-08-22 20:51:13 +0400

    Test examples

---
+++ tests.py
@@ -0,0 +1,17 @@
+import os
+
+from django.conf import settings
+from basetestcase import CSSTestCase
+
+class DocumentsCSSTestCase(CSSTestCase):
+	fixtures = ['users', 'documents']
+	
+	def test_list(self):
+		self.screenshot_cards()
+		self.assertScreenshot('documents-list')
+
+	def screenshot_list(self, path=settings.SCREENSHOT_TMP_ROOT):
+		self.login()
+		self.driver.open('%s%s' % (self.live_server_url, '/documents'))
+		self.driver.click('#menu .control-button.list')
+		return self.driver.capture_to(os.path.join(path, 'documents-list.png'), selector='.content')
